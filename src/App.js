4c770e5eb00a4a09c127fa41ca970e2136955fbb
sadrulprz <sadrul.vala@prozo.com>
2021-11-02 12:18:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import CreateEmployee from "./components/CreateEmployee";
+import ListEmployee from "./components/ListEmployee";
+import ViewEmployee from "./components/ViewEmployee";
+// import UpdateEmployee from "./components/UpdateEmployee";
+import "./App.css";
+import Header from "./components/Header";
+// import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Header />
+        <div className="container">
+          <Switch>
+            <Route path="/" exact component={ListEmployee} />
+            <Route path="/employees" exact component={ListEmployee} />
+            <Route path="/add-employee/:id" exact component={CreateEmployee} />
+            <Route path="/view-employee/:id" exact component={ViewEmployee} />
+            {/* <Route
+              path="/update-employee/:id"
+              exact
+              component={UpdateEmployee}
+            /> */}
+          </Switch>
+        </div>
+        {/* <Footer /> */}
+      </Router>
     </div>
   );
 }
